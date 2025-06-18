3f7aff96b86c6791ba430a14573afb54e83c242d
nwo5-trg <dogamations@gmail.com>
2025-06-18 18:38:21 -0400

    3:

--- changelog.md
+++ changelog.md
@@ -1,3 +1,9 @@
+## v1.0.3
+i just gotta quit geode modding and learn actual programming for a year
+
+## v1.0.2
+- **Fixed** render showing when particle icons are hidden
+
 ## v1.0.1
 - **Added** source
 
--- src/main.cpp
+++ src/main.cpp
@@ -49,92 +49,93 @@ class $modify(LevelEditorLayer) {
         auto particleObjs = ccArrayToVector<ParticleGameObject*>(m_particleObjects);
         if (particleObjs.empty()) return;
         auto fields = m_fields.self();
-        if (auto drawNode = fields->particleBoxDrawNode){
-            drawNode->clear();
-            auto col1 = fields->col1;
-            auto col2 = fields->col2;
-            auto col3 = fields->col1ForCenterLines ? col1 : fields->col3;
-            auto zoom = fields->batchLayer->getScale();
-            auto thickness = fields->thickness / zoom;
-            auto centerLineThickness = fields->centerLineThickness / zoom;
-            auto fill = fields->fill;
-            auto fillForRect = fields->fillForRect;
-            auto linesToCenter = fields->linestoCenter;
-            auto hideOnNonLayer = fields->hideOnNonLayer;
-            auto currentLayer = m_currentLayer;
-            for (auto obj : particleObjs) {
-                auto objCol1 = col1;
-                auto objCol2 = col2;
-                auto objCol3 = col3;
-                if (!(currentLayer == -1 || obj->m_editorLayer == currentLayer || obj->m_editorLayer == currentLayer)) {
-                    if (hideOnNonLayer) continue;
-                    else {
-                        objCol1 = ccc4f(col1.r, col1.g, col1.b, col1.a * 0.35f);
-                        objCol2 = ccc4f(col2.r, col2.g, col2.b, col2.a * 0.35f);
-                        objCol3 = ccc4f(col3.r, col3.g, col3.b, col3.a * 0.35f);
-                    }
-                }
-                std::vector<std::string> particleData;
-                std::stringstream ss(obj->m_particleData);
-                std::string str;
-                while (std::getline(ss, str, 'a')) particleData.push_back(str); // robtop stores particle shit in this string, its just particle struct but seperated with "a"s
-
-                auto pos = obj->getPosition();
-                auto scale = ccp(obj->m_scaleX, obj->m_scaleY);
-                auto maxScale = std::max(scale.x, scale.y);
-
-                auto radial = std::stoi(particleData[51]) == 1;
-
-                if (radial) {
-                    auto startRad = std::stof(particleData[45]) * maxScale;
-                    auto endRad = std::stof(particleData[47]) * maxScale;
-                    auto startRadVar = std::stof(particleData[46]) * maxScale;
-                    auto endRadVar = std::stof(particleData[48]) * maxScale;
-
-                    if (endRad == 0.0f && endRadVar != 0.0f) drawNode->drawCircle(pos, endRadVar, ccc4f(objCol2.r, objCol2.g, objCol2.b, objCol2.a * fill), thickness, objCol2, 100); // negative thickness not a great idea
-                    else if (endRad != 0.0f) {
-                        if (endRadVar != 0.0f) for (auto radius : {endRad + endRadVar, endRad - endRadVar}) drawNode->drawCircle(pos, radius, emptyCCC4F, thickness, objCol2, 100);
-                        drawNode->drawCircle(pos, endRad, emptyCCC4F, (endRadVar != 0.0f ? endRadVar : 1.0f), ccc4f(objCol2.r, objCol2.g, objCol2.b, objCol2.a * fill), 100);
-                    } else drawNode->drawCircle(pos, 2.5f, objCol2, 0.0f, emptyCCC4F, 25); // hard coded thing to show when radius and var are 0
-
-                    if (startRad == 0.0f && startRadVar != 0.0f) drawNode->drawCircle(pos, startRadVar, ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * fill), thickness, objCol1, 100);
-                    else if (startRad != 0.0f) {
-                        if (startRadVar != 0.0f) for (auto radius : {startRad + startRadVar, startRad - startRadVar}) drawNode->drawCircle(pos, radius, emptyCCC4F, thickness, objCol1, 100);
-                        drawNode->drawCircle(pos, startRad, emptyCCC4F, (startRadVar != 0.0f ? startRadVar : 1.0f), ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * fill), 100);
-                    } else drawNode->drawCircle(pos, 2.5f, objCol1, 0.0f, emptyCCC4F, 25);
-                    
-                    if (linesToCenter) {
-                        auto lineDistance = std::max(startRad + startRadVar, endRad + endRadVar) * 0.707106; // close enough this saves a calculation
-                        for (auto pos2 : {ccp(pos.x + lineDistance, pos.y + lineDistance), ccp(pos.x - lineDistance, pos.y + lineDistance), ccp(pos.x - lineDistance, pos.y - lineDistance), ccp(pos.x + lineDistance, pos.y -lineDistance)}) drawNode->drawSegment(pos, pos2, centerLineThickness, objCol3);
-                    }
-                }
+        auto drawNode = fields->particleBoxDrawNode;
+        if (!fields->particleBoxDrawNode) return;
+        drawNode->clear();
+        if (m_hideParticleIcons || m_alwaysHideParticleIcons) return;
+        auto col1 = fields->col1;
+        auto col2 = fields->col2;
+        auto col3 = fields->col1ForCenterLines ? col1 : fields->col3;
+        auto zoom = fields->batchLayer->getScale();
+        auto thickness = fields->thickness / zoom;
+        auto centerLineThickness = fields->centerLineThickness / zoom;
+        auto fill = fields->fill;
+        auto fillForRect = fields->fillForRect;
+        auto linesToCenter = fields->linestoCenter;
+        auto hideOnNonLayer = fields->hideOnNonLayer;
+        auto currentLayer = m_currentLayer;
+        for (auto obj : particleObjs) {
+            auto objCol1 = col1;
+            auto objCol2 = col2;
+            auto objCol3 = col3;
+            if (!(currentLayer == -1 || obj->m_editorLayer == currentLayer || obj->m_editorLayer == currentLayer)) {
+                if (hideOnNonLayer) continue;
                 else {
-                    auto rotation = -obj->getRotation();
-                    auto xRange = std::stof(particleData[9]) * scale.x;
-                    auto yRange = std::stof(particleData[10]) * scale.y;
-
-                    CCPoint points[4] = {
-                        ccp(pos.x + xRange, pos.y + yRange),
-                        ccp(pos.x - xRange, pos.y + yRange),
-                        ccp(pos.x - xRange, pos.y - yRange),
-                        ccp(pos.x + xRange, pos.y - yRange)
-                    };
-
-                    if (rotation == 0);
-                    else { // learning trigonometry for ts smh
-                        float rad = rotation * (M_PI / 180.0f);
-                        auto cos = std::cos(rad);
-                        auto sin = std::sin(rad);
-                        for (int i = 0; i < 4; i++) {
-                            auto& point = points[i];
-                            auto dx = point.x - pos.x;
-                            auto dy = point.y - pos.y;
-                            point = ccp(pos.x + ((dx * cos) - (dy * sin)), pos.y + ((dx * sin) + (dy * cos)));
-                        }
-                    }
-                    drawNode->drawPolygon(points, 4, ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * (fillForRect ? fill : 0.0f)), thickness, objCol1); 
-                    if (linesToCenter) for (int i = 0; i < 4; i++) drawNode->drawSegment(points[i], pos, thickness, objCol3);
+                    objCol1 = ccc4f(col1.r, col1.g, col1.b, col1.a * 0.35f);
+                    objCol2 = ccc4f(col2.r, col2.g, col2.b, col2.a * 0.35f);
+                    objCol3 = ccc4f(col3.r, col3.g, col3.b, col3.a * 0.35f);
                 }
+            }
+            std::vector<std::string> particleData;
+            std::stringstream ss(obj->m_particleData);
+            std::string str;
+            while (std::getline(ss, str, 'a')) particleData.push_back(str); // robtop stores particle shit in this string, its just particle struct but seperated with "a"s
+
+            auto pos = obj->getPosition();
+            auto scale = ccp(obj->m_scaleX, obj->m_scaleY);
+            auto maxScale = std::max(scale.x, scale.y);
+
+            auto radial = std::strtol(particleData[51].c_str(), nullptr, 10) == 1;
+
+            if (radial) {
+                auto startRad = std::strtof(particleData[45].c_str(), nullptr) * maxScale;
+                auto endRad = std::strtof(particleData[47].c_str(), nullptr) * maxScale;
+                auto startRadVar = std::strtof(particleData[46].c_str(), nullptr) * maxScale;
+                auto endRadVar = std::strtof(particleData[48].c_str(), nullptr) * maxScale;
+
+                if (endRad == 0.0f && endRadVar != 0.0f) drawNode->drawCircle(pos, endRadVar, ccc4f(objCol2.r, objCol2.g, objCol2.b, objCol2.a * fill), thickness, objCol2, 100); // negative thickness not a great idea
+                else if (endRad != 0.0f) {
+                    if (endRadVar != 0.0f) for (auto radius : {endRad + endRadVar, endRad - endRadVar}) drawNode->drawCircle(pos, radius, emptyCCC4F, thickness, objCol2, 100);
+                    drawNode->drawCircle(pos, endRad, emptyCCC4F, (endRadVar != 0.0f ? endRadVar : 1.0f), ccc4f(objCol2.r, objCol2.g, objCol2.b, objCol2.a * fill), 100);
+                } else drawNode->drawCircle(pos, 2.5f, objCol2, 0.0f, emptyCCC4F, 25); // hard coded thing to show when radius and var are 0
+
+                if (startRad == 0.0f && startRadVar != 0.0f) drawNode->drawCircle(pos, startRadVar, ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * fill), thickness, objCol1, 100);
+                else if (startRad != 0.0f) {
+                    if (startRadVar != 0.0f) for (auto radius : {startRad + startRadVar, startRad - startRadVar}) drawNode->drawCircle(pos, radius, emptyCCC4F, thickness, objCol1, 100);
+                    drawNode->drawCircle(pos, startRad, emptyCCC4F, (startRadVar != 0.0f ? startRadVar : 1.0f), ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * fill), 100);
+                } else drawNode->drawCircle(pos, 2.5f, objCol1, 0.0f, emptyCCC4F, 25);
+                
+                if (linesToCenter) {
+                    auto lineDistance = std::max(startRad + startRadVar, endRad + endRadVar) * 0.707106; // close enough this saves a calculation
+                    for (auto pos2 : {ccp(pos.x + lineDistance, pos.y + lineDistance), ccp(pos.x - lineDistance, pos.y + lineDistance), ccp(pos.x - lineDistance, pos.y - lineDistance), ccp(pos.x + lineDistance, pos.y -lineDistance)}) drawNode->drawSegment(pos, pos2, centerLineThickness, objCol3);
+                }
+            }
+            else {
+                auto rotation = -obj->getRotation();
+                auto xRange = std::strtof(particleData[9].c_str(), nullptr) * scale.x;
+                auto yRange = std::strtof(particleData[10].c_str(), nullptr) * scale.y;
+
+                CCPoint points[4] = {
+                    ccp(pos.x + xRange, pos.y + yRange),
+                    ccp(pos.x - xRange, pos.y + yRange),
+                    ccp(pos.x - xRange, pos.y - yRange),
+                    ccp(pos.x + xRange, pos.y - yRange)
+                };
+
+                if (rotation == 0);
+                else { // learning trigonometry for ts smh
+                    float rad = rotation * (M_PI / 180.0f);
+                    auto cos = std::cos(rad);
+                    auto sin = std::sin(rad);
+                    for (int i = 0; i < 4; i++) {
+                        auto& point = points[i];
+                        auto dx = point.x - pos.x;
+                        auto dy = point.y - pos.y;
+                        point = ccp(pos.x + ((dx * cos) - (dy * sin)), pos.y + ((dx * sin) + (dy * cos)));
+                    }
+                }
+                drawNode->drawPolygon(points, 4, ccc4f(objCol1.r, objCol1.g, objCol1.b, objCol1.a * (fillForRect ? fill : 0.0f)), thickness, objCol1); 
+                if (linesToCenter) for (int i = 0; i < 4; i++) drawNode->drawSegment(points[i], pos, thickness, objCol3);
             }
         }
     }
